10e8da002c6276dcadadd53d824263d6fbbc388c
Anusha Tomar <anushatoma913@gmail.com>
2025-02-16 05:12:43 +0530

    completed

--- redux-toolkit/src/main.jsx
+++ redux-toolkit/src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './index.css'
+import { Provider } from 'react-redux'
+import {store} from './app/store'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
     <App />
-  </StrictMode>,
+  </Provider>,
 )
+
+
+// see : https://youtu.be/1i04-A7kfFI?list=PLu71SKxNbfoDqgPchmvIsL4hTnJIrtige&t=3662
